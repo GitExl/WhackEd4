684b92d317b7c7d74c3982bbdade400b621196e5
GitExl <dennis.meuwissen@gmail.com>
2015-04-25 15:27:31 +0200

    Bump version to 1.1.0.

--- src/whacked4/config.py
+++ src/whacked4/config.py
@@ -12,7 +12,7 @@ import os.path
 # Application info.
 APP_NAME = 'WhackEd4'
 APP_SIMPLE_NAME = 'whacked4'
-APP_VERSION = '1.0.5'
+APP_VERSION = '1.1.0'
 
 # Monospaced font for displays that use them.
 FONT_MONOSPACED_NAME = 'Bitstream Vera Sans Mono'
@@ -38,12 +38,12 @@ class WhackEd4Settings(settingshandler.SettingsHandler):
     """
     Settings for WhackEd4.
     """
-    
+
     def __init__(self, path):
         settingshandler.SettingsHandler.__init__(self, path)
-        
+
         self.recent_files_clean()
-    
+
     def register(self):
         self.register_setting('main_window_state', {
             'x': 0,
@@ -55,12 +55,12 @@ class WhackEd4Settings(settingshandler.SettingsHandler):
         self.register_setting('recent_files', [])
         self.register_setting('undo_size', 256)
         self.register_setting('recent_files_count', 10)
-        
+
     def main_window_state_store(self, x, y, width, height, is_maximized):
         """
         Stores the state of the main window.
         """
-        
+
         main_window = {
             'x': x,
             'y': y,
@@ -69,14 +69,14 @@ class WhackEd4Settings(settingshandler.SettingsHandler):
             'maximized': is_maximized
         }
         self.put_setting('main_window_state', main_window)
-        
+
     def main_window_state_restore(self, window):
         """
         Restores the main window's state from these settings.
         """
-        
+
         main_window = self.get_setting('main_window_state')
-        
+
         window.SetDimensions(
             main_window['x'],
             main_window['y'],
@@ -85,40 +85,40 @@ class WhackEd4Settings(settingshandler.SettingsHandler):
         )
         window.Maximize(main_window['maximized'])
         window.Refresh()
-     
+
     def recent_files_add(self, filename):
         """
         Adds a filename to the list of recent filenames.
         """
-        
+
         recent_files = self.get_setting('recent_files')
-        
+
         # Remove the file from the list if it already exists elsewhere.
         if filename in recent_files:
             recent_files.remove(filename)
-        
+
         # Trim the list if it has reached it's maximum length.
         if len(recent_files) > self['recent_files_count']:
             del recent_files[len(recent_files) - 1]
-            
+
         recent_files.insert(0, filename)
-        
+
         self.put_setting('recent_files', recent_files)
-    
+
     def recent_files_clean(self):
         """
         Filters missing files from the recent files list.
         """
-        
+
         recent_files = self.get_setting('recent_files')
-        
+
         new_recent = []
         for filename in recent_files:
             if os.path.exists(filename) and not filename.lower() in map(unicode.lower, new_recent):
                 new_recent.append(filename)
-        
-        self.put_setting('recent_files', new_recent)   
-        
+
+        self.put_setting('recent_files', new_recent)
+
 
 # Global settings object.
 settings = WhackEd4Settings(SETTINGS_PATH)
