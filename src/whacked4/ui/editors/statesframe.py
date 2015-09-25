d341ac9c5d38ed949ed680187d8fb8b84d83b0b1
GitExl <dennis.meuwissen@gmail.com>
2015-09-25 18:43:20 +0200

    Fix state editor default column widths

--- src/whacked4/ui/editors/statesframe.py
+++ src/whacked4/ui/editors/statesframe.py
@@ -313,12 +313,12 @@ class StatesFrame(editormixin.EditorMixin, windows.StatesFrameBase):
 
         # Add list column headers if needed.
         if self.StateList.GetColumnCount() == 0:
-            self.StateList.InsertColumn(0, '', width=29)
+            self.StateList.InsertColumn(0, '', width=37)
             self.StateList.InsertColumn(1, 'Name', width=47)
             self.StateList.InsertColumn(2, 'Spr', width=33)
             self.StateList.InsertColumn(3, 'Frm', width=34)
             self.StateList.InsertColumn(4, 'Lit', width=25)
-            self.StateList.InsertColumn(5, 'Next', width=36)
+            self.StateList.InsertColumn(5, 'Next', width=40)
             self.StateList.InsertColumn(6, 'Dur', width=40)
             self.StateList.InsertColumn(7, 'Action', width=160)
             self.StateList.InsertColumn(8, 'Parameters', width=107)
