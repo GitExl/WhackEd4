1e1f75f5004a5699fe25c406e1dd036bfb6e308f
GitExl <GitExl@users.noreply.github.com>
2018-07-24 08:21:22 +0200

    Enable high DPI suport if available.

--- src/main.py
+++ src/main.py
@@ -1,9 +1,19 @@
 #!/usr/bin/env python
 #coding=utf8
 
+import platform
+
 from whacked4 import app
+from ctypes import windll
 
 
 if __name__ == '__main__':
+
+    # Enable better High DPI support for Windows Vista and later.
+    if platform.system() == 'Windows':
+        win_version = platform.version().split('.')
+        if int(win_version[0]) >= 6:
+            windll.user32.SetProcessDPIAware()
+
     main_app = app.WhackEd4App()
     main_app.MainLoop()
